407f7b3a5513c5cdf472f466a4f2f95dd0f0e07f
Andrii Hromanchuk <73118147+TrueMage@users.noreply.github.com>
2023-11-06 14:55:56 +0100

    update

--- WF-DrawButton/Form1.cs
+++ WF-DrawButton/Form1.cs
@@ -12,28 +12,32 @@ namespace WF_DrawButton
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
+            if (!startPoint.IsEmpty) return;
             startPoint = e.Location;
+
+            Button temp = new Button();
+            temp.Location = startPoint;
+
+            temp.Width = 0;
+            temp.Height = 0;
+            temp.Text = buttons.Count.ToString();
+
+            this.Controls.Add(temp);
+            buttons.Add(temp);
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             if (startPoint.IsEmpty) return;
 
-            Button temp = new Button();
-
-            temp.Location = startPoint;
-            temp.Width = startPoint.X - e.X;
-            temp.Height = startPoint.Y - e.Y;
-            temp.Text = buttons.Count.ToString();
-
-            this.Controls.Add(temp);
-
-            buttons.Add(temp);
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-8.0/ranges
+            buttons[^1].Width = e.X - startPoint.X;
+            buttons[^1].Height = e.Y - startPoint.Y;
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-
+            startPoint = Point.Empty;
         }
     }
 }
